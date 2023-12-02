13866d8a51f868b760259787114fe0ab8ba02cc6
Alexey Artyushin <shinxey1@gmail.com>
2023-12-02 20:46:37 +0600

    haskell day 2 refactoring

--- haskell/app/Day2.hs
+++ haskell/app/Day2.hs
@@ -1,5 +1,6 @@
 module Day2 where
 
+import Data.Maybe
 import Data.List
 import Data.List.Split
 
@@ -7,26 +8,23 @@ colors :: [String]
 colors = ["red", "green", "blue"]
 
 colorIdx :: String -> Int
-colorIdx string = 
-    case find (\(_, x) -> x == string) (zip [0..] colors) of
-        Just (idx, _) -> idx
-        Nothing -> -1
+colorIdx string = fromJust $ elemIndex string colors
 
 setListElem :: Int -> a -> [a] -> [a]
 setListElem idx newElem list = take idx list ++ [newElem] ++ drop (idx + 1) list
 
-extractCubesRecursion :: [Int] -> [String] -> [Int]
-extractCubesRecursion acc [] = acc
-extractCubesRecursion acc (x:xs) = 
+extractCubes :: [Int] -> [String] -> [Int]
+extractCubes acc [] = acc
+extractCubes acc (x:xs) = 
     let components = splitOn " " x
         colorIndex = colorIdx (components!!1)
         cubesCount = acc!!colorIndex
-        newCubesCount = read (components!!0) :: Int
+        newCubesCount = read (head components) :: Int
         newAcc = setListElem colorIndex (max cubesCount newCubesCount) acc
-    in extractCubesRecursion newAcc xs
+    in extractCubes newAcc xs
 
 extractMaxCubes :: String -> [Int]
-extractMaxCubes = extractCubesRecursion (map (\_ -> 0) colors) . concatMap (splitOn ", ") . splitOn "; "
+extractMaxCubes = extractCubes (map (const 0) colors) . concatMap (splitOn ", ") . splitOn "; "
 
 gamePower :: String -> Int
 gamePower = product . extractMaxCubes . last . splitOn ": "
