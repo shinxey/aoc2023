743230671d906bfd35cc6f7c5b225d49b3ffaf57
Alexey Artyushin <shinxey1@gmail.com>
2023-12-18 16:32:25 +0600

    haskell day 3

---
+++ haskell/app/Day3.hs
@@ -0,0 +1,71 @@
+module Day3 where
+
+import Data.Char (isDigit)
+import qualified Data.Char as Char
+
+-- context
+
+data Part = Part { position :: Int, len :: Int, value :: Int }
+data Context = Context { parts :: [[Part]], gears :: [[Part]] }
+
+instance Show Part where
+    show (Part pos len val) = "{ pos: " ++ show pos ++ ", len: " ++ show len ++ ", value: " ++ show val ++ "}"
+
+emptyContext :: Context
+emptyContext = Context [[], [], []] [[], [], []]
+
+-- parsing
+
+parseParts :: String -> Int -> [Part]
+parseParts [] _ = []
+parseParts str pos =
+    let chars = takeWhile Char.isDigit str in
+    case chars of
+        [] -> parseParts (dropWhile (not . Char.isDigit) str) (pos + length (takeWhile (not . Char.isDigit) str))
+        _ -> Part pos (length chars) (read chars :: Int):parseParts (dropWhile Char.isDigit str) (pos + length chars)
+
+parseGears :: String -> Int -> [Part]
+parseGears [] _ = []
+parseGears (x:xs) pos
+    | x == '*' =
+        let newPart = Part pos 1 1
+        in
+        newPart:parseGears xs (pos + 1)
+    | otherwise = parseGears xs (pos + 1)
+
+-- solving
+
+shiftContext :: Context -> String -> Context
+shiftContext (Context parts gears) line =
+    let newParts = parseParts line 0
+        newGears = parseGears line 0
+    in
+    Context (drop 1 parts ++ [newParts]) (drop 1 gears ++ [newGears])
+
+gearMatchesPart :: Part -> Part -> Bool
+gearMatchesPart gear part =
+    let gearPos = position gear
+        partStart = position part - 1
+        partEnd = partStart + len part + 1
+    in
+    gearPos >= partStart && gearPos <= partEnd
+
+countGearRatios :: [[Part]] -> Part -> Int
+countGearRatios parts gear =
+    let matchingParts = filter (gearMatchesPart gear) . concat $ parts
+    in
+    if length matchingParts /= 2 then 0 else product . map value $ matchingParts
+
+countRatios :: Context -> Int
+countRatios (Context parts gears) = sum . map (countGearRatios parts) $ gears!!1
+
+day3Solve :: Context -> [String] -> Int
+day3Solve _ [] = 0
+day3Solve context (line:lines) =
+    let newContext = shiftContext context line
+    in
+    countRatios newContext + day3Solve newContext lines
+
+day3 :: String -> Int
+day3 content = day3Solve emptyContext (lines content)
+
--- haskell/app/Main.hs
+++ haskell/app/Main.hs
@@ -1,10 +1,13 @@
 import Day1
 import Day2
+import Day3
 
 main = do
     day1Input <- readFile "inputs/input1.txt"
     putStrLn ("day 1 answer is " ++ (show . day1 $ day1Input))
     day2Input <- readFile "inputs/input2.txt"
     putStrLn ("day 2 answer is " ++ (show . day2 $ day2Input))
+    day3Input <- readFile "inputs/input3.txt"
+    putStrLn ("day 3 answer is " ++ (show . day3 $ day3Input))
     return ()
 
